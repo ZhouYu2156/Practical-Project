8352510bc6f141c2095aa21f556abf3ff84b0637
ZhouYu2156 <1043744584@qq.com>
2024-10-11 12:04:28 +0800

    自动提交脚本

--- dev.sh
+++ dev.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 set -e
+# 检查是否提供了提交信息
+if [ "$#" -eq 0 ]; then
+    # 如果没有提供提交信息，提示用户输入
+    read -p "请输入提交信息: " commit_msg
+else
+    # 如果提供了提交信息，使用第一个参数作为提交信息
+    commit_msg="$1"
+fi
 
-function commit_git() {
-  if [ -z "$1" ]; then
-    echo "请输入提交信息"
-    read -r commit_msg
-    git add .
-    git commit -m "$commit_msg"
-    git push
-  else
-    git add .
-    git commit -m "$1"
-    git push
-  fi
-}
-
-commit_git "$1"
+# 执行git提交命令
+git add .
+git commit -m "$commit_msg"
+# 提示提交结果
+echo "提交成功: $commit_msg"
+git push githubware main
+echo "推送成功"
